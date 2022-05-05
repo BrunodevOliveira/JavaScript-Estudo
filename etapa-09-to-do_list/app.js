5f1d29089b4d25bfe80897ffc0d45a0e9d6ad52c
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-05-05 13:24:59 -0300

    Filtragem de to-dos criada e código refatorado

--- etapa-09-to-do_list/app.js
+++ etapa-09-to-do_list/app.js
@@ -2,44 +2,64 @@ const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
 const inputSearchTodo = document.querySelector('.form-search input')
 
-formAddTodo.addEventListener('submit', event => {
-  event.preventDefault()
-  
-  const newTodo = document.createElement('li')
+const removeTodo = event => {
+  const clickedElement = event.target
+  const trashDataValue = clickedElement.dataset.trash
+  const todo = document.querySelector(`[data-todo="${trashDataValue}"]`)
 
+  if(trashDataValue){
+    todo.remove()
+  }
+}
+
+const addTodo = event => {
+  event.preventDefault()
   const inputValue = event.target.add.value.trim()
 
   if(inputValue.length) {
-    newTodo.innerHTML =  `<span>${inputValue}</span> <i class="far fa-trash-alt delete"></i>`
-    newTodo.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
-    todosContainer.append(newTodo)  
+    todosContainer.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+      <span>${inputValue}</span>
+      <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
+    </li>
+    `
     event.target.reset()
   }
-})
+}
 
-todosContainer.addEventListener('click', event => {
-  const clickedElement = event.target
-  const classDeleteExist = [...clickedElement.classList].includes('delete')
+const filterTodos = (todos, inputValue, returnMatchedTodos) => todos
+  .filter(todo => {
+    const matchedTodos = todo.textContent.toLowerCase().includes(inputValue)
+    return returnMatchedTodos ? matchedTodos : !matchedTodos
+  })
 
-  if(classDeleteExist){
-    clickedElement.parentElement.remove()
-  }
-})
+const manipulateClasses = (todos, classToAdd, classToRemove) => {
+  todos.forEach(todo => {
+    todo.classList.remove(classToRemove)
+    todo.classList.add(classToAdd)
+  })
+}
 
-inputSearchTodo.addEventListener('input', event => {
+const hideTodos = (todos, inputValue) => {
+  // Deixa os Todos que não dão match com o valor pesquisado invisíveis
+  const todoToHide = filterTodos(todos, inputValue, false)
+  manipulateClasses(todoToHide, 'd-none', 'd-flex')
+}
+
+const showTodos = (todos, inputValue) => {
+  // Deixa os Todos que dão match com o valor pesquisado visíveis
+  const todoToShow = filterTodos(todos, inputValue, true)
+  manipulateClasses(todoToShow, 'd-flex', 'd-none')
+}
+
+const searchTodo = event => {
   const inputValue = event.target.value.trim().toLowerCase()
+  const todos = Array.from(todosContainer.children)
 
-  Array.from(todosContainer.children)
-    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue)) //As que não contem o valor do input
-    .forEach(todo => {
-      todo.classList.remove('d-flex')
-      todo.classList.add('d-none')
-    })
-  
-  Array.from(todosContainer.children)
-    .filter(todo => todo.textContent.toLowerCase().includes(inputValue)) //As que contém o valor do input
-    .forEach(todo => {
-      todo.classList.remove('d-none')
-      todo.classList.add('d-flex')
-    })
-})
+  hideTodos(todos, inputValue)
+  showTodos(todos, inputValue)
+}
+
+formAddTodo.addEventListener('submit', addTodo)
+todosContainer.addEventListener('click', removeTodo)
+inputSearchTodo.addEventListener('input', searchTodo)
