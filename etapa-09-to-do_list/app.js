05cb328eee2a3c6a87b5cf5d0904541ff862f829
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-05-05 01:38:38 -0300

    filtro To-do list feito

--- etapa-09-to-do_list/app.js
+++ etapa-09-to-do_list/app.js
@@ -1,5 +1,6 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
+const inputSearchTodo = document.querySelector('.form-search input')
 
 formAddTodo.addEventListener('submit', event => {
   event.preventDefault()
@@ -11,7 +12,34 @@ formAddTodo.addEventListener('submit', event => {
   if(inputValue.length) {
     newTodo.innerHTML =  `<span>${inputValue}</span> <i class="far fa-trash-alt delete"></i>`
     newTodo.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
-    todosContainer.prepend(newTodo)  
+    todosContainer.append(newTodo)  
     event.target.reset()
   }
+})
+
+todosContainer.addEventListener('click', event => {
+  const clickedElement = event.target
+  const classDeleteExist = [...clickedElement.classList].includes('delete')
+
+  if(classDeleteExist){
+    clickedElement.parentElement.remove()
+  }
+})
+
+inputSearchTodo.addEventListener('input', event => {
+  const inputValue = event.target.value.trim().toLowerCase()
+
+  Array.from(todosContainer.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(inputValue)) //As que não contem o valor do input
+    .forEach(todo => {
+      todo.classList.remove('d-flex')
+      todo.classList.add('d-none')
+    })
+  
+  Array.from(todosContainer.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(inputValue)) //As que contém o valor do input
+    .forEach(todo => {
+      todo.classList.remove('d-none')
+      todo.classList.add('d-flex')
+    })
 })
