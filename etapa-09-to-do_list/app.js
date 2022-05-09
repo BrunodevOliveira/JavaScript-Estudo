9b23353d86f65ed855e1d313052742480417e040
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-05-09 16:19:04 -0300

    to-do list testes

--- etapa-09-to-do_list/app.js
+++ etapa-09-to-do_list/app.js
@@ -1,6 +1,10 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
 const inputSearchTodo = document.querySelector('.form-search input')
+const body = document.querySelector('body')
+
+const editbutton = document.querySelector("[data-edit='Assistir Breaking Bad']")
+const paragraph = document.createElement('p')
 
 const removeTodo = event => {
   const clickedElement = event.target
@@ -12,17 +16,21 @@ const removeTodo = event => {
   }
 }
 
+
 const addTodo = event => {
   event.preventDefault()
   const inputValue = event.target.add.value.trim()
 
-  if(inputValue.length) {
-    todosContainer.innerHTML += `
+  const todoExist = [...todosContainer.children].some(({ outerText }) => outerText === inputValue)
+  
+  const todoTemplate = `
     <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
       <span>${inputValue}</span>
       <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
-    </li>
-    `
+    </li>`
+
+  if(inputValue.length) {
+    !todoExist ? todosContainer.innerHTML += todoTemplate : createAlertMessage()
     event.target.reset()
   }
 }
@@ -50,6 +58,35 @@ const showTodos = (todos, inputValue) => {
   // Deixa os Todos que dão match com o valor pesquisado visíveis
   const todoToShow = filterTodos(todos, inputValue, true)
   manipulateClasses(todoToShow, 'd-flex', 'd-none')
+  insertMessageTodoNotFound(todoToShow)
+}
+
+const insertMessageTodoNotFound = ({ length: quantityOfTodos }) => {
+  const messageTodoNotExist = 'Não existe nenhum To-do na lista com essa palavra'
+  const liTodo = document.querySelectorAll(`[data-todo="${messageTodoNotExist}"]`)
+  
+  if(!quantityOfTodos){
+    todosContainer.innerHTML += `
+    <li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${messageTodoNotExist}">
+      <span>${messageTodoNotExist}</span>
+    </li>`
+  } 
+  if(quantityOfTodos && liTodo){
+    liTodo.forEach( li => li.remove())
+  }
+}
+
+const createAlertMessage = () => {
+  const label = document.querySelector('.msg-error')
+  
+  paragraph.textContent= `Todo já existe na lista`
+  paragraph.classList.add('alert', 'alert-primary')
+  paragraph.setAttribute('role', 'alert')
+  label.insertAdjacentElement('afterend',paragraph )
+  
+  body.addEventListener('click', () => {
+    document.querySelector('.alert').style.display = 'none'
+  })
 }
 
 const searchTodo = event => {
@@ -63,3 +100,29 @@ const searchTodo = event => {
 formAddTodo.addEventListener('submit', addTodo)
 todosContainer.addEventListener('click', removeTodo)
 inputSearchTodo.addEventListener('input', searchTodo)
+
+
+todosContainer.addEventListener('click', event => {
+  const clickedElement = event.target
+  const editDataValue = clickedElement.dataset.edit
+  const contentTodo = clickedElement.parentElement.previousElementSibling
+  console.log(editDataValue)
+  if(editDataValue) {
+    document.querySelector('.popup-wrapper').style.display = 'block'
+  }
+})
+
+const closePopupEditTodo = document.querySelector(`[data-close]`)
+
+closePopupEditTodo.addEventListener('click', () => {
+  document.querySelector('.popup-wrapper').style.display = 'none'
+})
+
+const editTodoPopup = document.querySelector(`[data-save]`)
+
+editTodoPopup.addEventListener('click', () => {
+
+  editTodoPopup.previousElementSibling.value
+  document.querySelector('.popup-wrapper').style.display = 'none'
+
+})
