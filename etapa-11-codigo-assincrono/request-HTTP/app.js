1929fe8e251a017b3b85b0cd64c150f4fd52f9e9
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-05-12 14:26:45 -0300

    Request com JSON parse e callback Hell

--- etapa-11-codigo-assincrono/request-HTTP/app.js
+++ etapa-11-codigo-assincrono/request-HTTP/app.js
@@ -1,12 +1,38 @@
-const request = new XMLHttpRequest()
-const endPoint = 'https://jsonplaceholder.typicode.com/todos'
+const getTodos = (url, callback) => {
+  const request = new XMLHttpRequest()
+  
+  request.addEventListener('readystatechange', () => {
+    const isRequestOk = request.readyState === 4 && request.status === 200
+    const isRequestNotOk = request.readyState === 4
+  
+    if(isRequestOk) {
+      const data = JSON.parse(request.responseText) //Recebe como argumento uma string JSON e parseia (converte) em um array de objetos
+      callback(null, data)
+      return
+    }
+    if(isRequestNotOk) {
+      callback('Não foi possível obter os dados', null)
+    }
+  })
+  // https://jsonplaceholder.typicode.com/todos
+  request.open('GET', url)
+  request.send()
+}
 
-request.addEventListener('readystatechange', () => {
-  if(request.readyState === 4 && request.status === 200) {
-    console.log(request)
-    console.log(request.responseText)
-  }
+console.log(1)
+console.log(2)
+
+// Essa operação por ser assíncrona não irá bloquear a thread, sendo executada no final.
+getTodos('./json/todos.json',(error, data) => {
+  console.log(data)
+  getTodos('./json/todos-02.json', (error, data) => {
+    console.log(data)
+    getTodos('./json/todos-02.json', (error, data) => {
+      console.log(data)
+    })
+  })
+  
 })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos')
-request.send()
+console.log(3)
+console.log(4)
