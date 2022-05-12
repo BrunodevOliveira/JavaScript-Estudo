c62e45b7f5c97b63e5817cbee165139e0d37470e
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-05-11 21:49:58 -0300

    criando primeira request

---
+++ etapa-11-codigo-assincrono/request-HTTP/app.js
@@ -0,0 +1,12 @@
+const request = new XMLHttpRequest()
+const endPoint = 'https://jsonplaceholder.typicode.com/todos'
+
+request.addEventListener('readystatechange', () => {
+  if(request.readyState === 4 && request.status === 200) {
+    console.log(request)
+    console.log(request.responseText)
+  }
+})
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todoss')
+request.send()
