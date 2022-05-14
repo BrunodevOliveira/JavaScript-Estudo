98f400ceccf3caaa1802942f840355486b05aa72
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-05-14 04:09:29 -0300

    Exercicio 30 feito

--- Exercicios/exercicio-30/app.js
+++ Exercicios/exercicio-30/app.js
@@ -8,6 +8,32 @@
     dados dos usuários."
 */
 
+const getUsers = url => new Promise((resolve, reject) => {
+  const request = new XMLHttpRequest()
+
+  request.addEventListener('readystatechange', () => {
+    const isRequestOk = request.status === 200 && request.readyState === 4
+    const isRequesyNotOk = request.readyState === 4
+
+    if (isRequestOk) {
+      const data = JSON.parse(request.responseText)
+
+      resolve(data)
+    }
+
+    if (isRequesyNotOk) {
+      reject('Não foi possível obter os dados dos usuários.')
+    }
+  })
+
+  request.open('GET', url)
+  request.send()
+})
+
+getUsers('https://jsonplaceholder.typicode.com/users')
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+
 /*
   02
 
@@ -22,6 +48,34 @@
   - Se o operador não for válido, retorne a mensagem "Operação inválida."
 */
 
+const getOperationMessage = (val1, operator, val2, operation) =>
+  `Resultado da operação: ${val1} ${operator} ${val2} = ${operation}.`
+
+const calculator = operator => (val1, val2) => {
+  const operations = {
+    '+': getOperationMessage(val1, operator, val2, val1 + val2),
+    '-': getOperationMessage(val1, operator, val2, val1 - val2),
+    '*': getOperationMessage(val1, operator, val2, val1 * val2),
+    '/': getOperationMessage(val1, operator, val2, val1 / val2),
+    '%': getOperationMessage(val1, operator, val2, val1 % val2),
+  }
+
+  return operations[operator] || "Operação inválida."
+}
+const sum = calculator('+')
+const subtraction = calculator('-')
+const multiplication = calculator('*')
+const division = calculator('/')
+const restDivision = calculator('%')
+const errorCalculator = calculator('**')
+
+// console.log(sum(20, 10))
+// console.log(subtraction(20, 10))
+// console.log(multiplication(20, 10))
+// console.log(division(20, 10))
+// console.log(restDivision(20, 10))
+// console.log(errorCalculator(20, 10))
+
 /*
   03
 
@@ -35,6 +89,19 @@
   - Crie um novo array chamado `newSul`, que recebe somente os estados do sul,
     pegando do array `brasil`. Não remova esses itens de `brasil`.
 */
+const sul = ['Paraná', 'Santa Catarina', 'Rio Grande do Sul']
+const sudeste = ['Rio de Janeiro', 'São Paulo', 'Minas Gerais', 'Espirito Santo']
+// const norte = ['Acre', 'Amapá', 'Amazonas']
+
+let brasil = sul.concat(sudeste)
+console.log(brasil)
+
+brasil.unshift('Acre', 'Amapá', 'Amazonas') //adiciona ao início do array
+console.log(brasil.shift()) //Remove o primeiro item do array e o retorna
+
+// const newSul = brasil.filter(estado => sul.includes(estado))
+const newSul = brasil.slice(2,5)
+console.log(newSul)
 
 /*
   04
@@ -54,6 +121,20 @@
     console: "Nem todos os estados tem mais de 7 letras.". Pesquise pelo método 
     every.
 */
+const nordeste = ['Bahia', 'Ceará', 'Maranhão', 'Paraíba', 'Piauí', 'Rio Grande do Norte', 'Pernambuco', 'Alagoas', 'Sergipe']
+const newSudeste = brasil.splice(5, 4)
+// brasil = brasil.concat(nordeste)
+nordeste.forEach(estado => brasil.push(estado))
+
+const newBrasil = brasil.map((estado, index) => ({ 'id': index,'estado': estado }))
+
+const hasMoreThan7Letters = brasil.every(estado => estado.length > 7)
+const message = hasMoreThan7Letters
+  ? "Sim, todos os estados tem mais de 7 letras." 
+  : "Nem todos os estados tem mais de 7 letras."
+console.log(message)
+
+console.log(newBrasil)
 
 /*
   05
@@ -68,3 +149,17 @@
   - Filtre o array criado acima, retornando somente os estados que tiverem ID 
     par. Atribua este novo array à uma constante.
 */
+const cearaInclude = brasil.includes('Ceará')
+const cearaMessage = cearaInclude ? "Ceará está incluído." : "Ceará não foi incluído =/"
+
+console.log(cearaMessage)
+
+const estadosDoBrasil = newBrasil.map(({ id, estado }) => ({
+  id: ++id,
+  estado: estado = `${estado} pertence ao Brasil`
+}))
+
+console.log(estadosDoBrasil)
+const estadosComIdPar = estadosDoBrasil.filter(estado => estado.id % 2 === 0)
+
+console.log(estadosComIdPar)
